3c333c3dfa919621f4e21692173eadae66c83c44
zainal-77 <zainal.726290@gmail.com>
2022-05-29 23:41:51 +0700

    continue building model

--- ModelBuilding.py
+++ ModelBuilding.py
@@ -39,3 +39,43 @@ val_gen = data_val.flow_from_directory("D:\BANGKIT\FaceEmotionDetection\gambar\T
                                                     batch_size=batch_size,
                                                     class_mode='categorical',
                                                     shuffle=False)
+                          
+
+                          
+#Model Building VGG16 is a convolution neural net (CNN ) architecture which was used to win ILSVR(Imagenet) competition in 2014. It is considered to be one of the excellent vision model architecture till date. Most unique thing about VGG16 is that instead of having a large number of hyper-parameter they focused on having convolution layers of 3x3 filter with a stride 1 and always used same padding and maxpool layer of 2x2 filter of stride 2. It follows this arrangement of convolution and max pool layers consistently throughout the whole architecture. In the end it has 2 FC(fully connected layers) followed by a softmax for output. The 16 in VGG16 refers to it has 16 layers that have weights. This network is a pretty large network and it has about 138 million (approx) parameters.          
+from keras.applications.vgg16 import VGG16
+
+base_model = VGG16(
+        weights=None,
+        include_top=False,
+        input_shape=img_size
+    )
+
+base_model.summary()
+                          
+                          
+X_train, y_train, train_labels = load_data(TRAIN_DIR, img_size)
+X_test, y_test, test_labels = load_data(TEST_DIR,img_size)
+                          
+epochs = 40
+batch_size = 64
+
+history = deep_model(model, X_train, Y_train, epochs, batch_size)
+                          
+#Menampilkan Hasil prediksi emotion                           
+from random import randint
+
+l = len(filenames)
+base_path = TEST_DIR
+for i in range(10):  # 10 images
+    
+    rnd_number = randint(0,l-1)
+    filename,pred_class,actual_class = pred_result.loc[rnd_number]
+    
+    img_path = os.path.join(base_path,filename)
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.title("Predicted Class: {} {} Actual Class: {}".format(pred_class,'\n',actual_class))
+    plt.show()
+    pass
