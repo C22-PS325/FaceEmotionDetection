90d746b0cfc6cde8ae738c612593fa268d6bb782
zainal-77 <zainal.726290@gmail.com>
2022-06-08 09:23:32 +0700

    Create for applying face detection

--- deploy_model/deploy_app.py
+++ deploy_model/deploy_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import cv2
 import numpy as np 
 from tensorflow.keras.models import load_model
+from tensorflow_addons.optimizers import AdamW
 
 app = Flask(__name__)
 
